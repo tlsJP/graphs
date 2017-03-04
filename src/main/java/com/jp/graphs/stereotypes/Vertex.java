e64917b6d95dc84d812311e9736e78d839ee326a
John Albert <jp@thejpaproject.com>
2017-03-03 22:24:03 -0800

    added equals and hashcode to vertex added constructor for vertex updated constructor for simplevertex

--- src/main/java/com/jp/graphs/stereotypes/Vertex.java
+++ src/main/java/com/jp/graphs/stereotypes/Vertex.java
@@ -4,7 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Core implementation of a vertex
+ * Core implementation of a vertex.
+ * I haven't decided yet how 'sentient' the vertices should be...
+ * <p>
  * <p>
  * Created by JP on 3/3/2017.
  */
@@ -13,6 +15,26 @@ public abstract class Vertex<T> {
     private T dataElement;
 
     private Set<Vertex> neighbors;
+
+    public Vertex() {
+        dataElement = null;
+        neighbors = new HashSet<>();
+    }
+
+    public Vertex(T dataElement) {
+        this();
+        this.dataElement = dataElement;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Vertex<?> vertex = (Vertex<?>) o;
+
+        return dataElement != null ? dataElement.equals(vertex.dataElement) : vertex.dataElement == null;
+    }
 
     public T getDataElement() {
         return dataElement;
@@ -26,6 +48,11 @@ public abstract class Vertex<T> {
         return neighbors;
     }
 
+    @Override
+    public int hashCode() {
+        return dataElement != null ? dataElement.hashCode() : 0;
+    }
+
     public boolean removeNeighbor(Vertex vertex) {
         return getNeighbors().remove(vertex);
     }
@@ -34,5 +61,10 @@ public abstract class Vertex<T> {
         this.dataElement = dataElement;
     }
 
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "["
+                + "dataElement:" + dataElement + "]";
+    }
 
 }
