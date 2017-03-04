578f0370317ebd15265f58240bc6f15dc5e1c868
John Albert <jp@thejpaproject.com>
2017-03-03 22:17:22 -0800

    mock out some sanity test

--- src/main/java/com/jp/graphs/client/Main.java
+++ src/main/java/com/jp/graphs/client/Main.java
@@ -1,23 +1,37 @@
 package com.jp.graphs.client;
 
+import com.jp.graphs.core.AdjacencyListGraph;
 import com.jp.graphs.core.SimpleVertex;
+import com.jp.graphs.stereotypes.Graph;
 import com.jp.graphs.stereotypes.Vertex;
+
+import java.util.stream.Collectors;
 
 /**
  * Runs all the thingies
- *
+ * <p>
  * Created by JP on 3/3/2017.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        SimpleVertex sv = new SimpleVertex();
-
-        Vertex v = new Vertex<Integer>(){};
+        Graph g = new AdjacencyListGraph();
 
 
+        Vertex a = new SimpleVertex(1);
+        Vertex b = new SimpleVertex(2);
+
+        g.add(a);
+        g.add(b);
+
+        g.connect(a, b);
+        System.out.println("a~b ? : " + g.isAdjacent(a, b));
+
+        g.disconnect(a, b);
+        System.out.println("a~b ? : " + g.isAdjacent(a, b));
 
 
+        System.out.println(g.toString());
     }
 }
