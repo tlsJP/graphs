a14fc280fccc4104d7d2dbdbdee45c719d36eedc
John Albert <jp@thejpaproject.com>
2017-03-03 22:16:50 -0800

    started filling in implementation for graph

--- src/main/java/com/jp/graphs/core/AdjacencyListGraph.java
+++ src/main/java/com/jp/graphs/core/AdjacencyListGraph.java
@@ -3,16 +3,21 @@ package com.jp.graphs.core;
 import com.jp.graphs.stereotypes.Graph;
 import com.jp.graphs.stereotypes.Vertex;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * TODO
- *
+ * <p>
  * Created by JP on 3/3/2017.
  */
 public class AdjacencyListGraph implements Graph {
 
+    private Set<Vertex> vertices = new HashSet<>();
+
     @Override
     public boolean add(Vertex vertex) {
-        return false;
+        return vertices.add(vertex);
     }
 
     @Override
@@ -26,12 +31,22 @@ public class AdjacencyListGraph implements Graph {
     }
 
     @Override
+    public Set<Vertex> getVertices() {
+        return vertices;
+    }
+
+    @Override
     public boolean isAdjacent(Vertex i, Vertex j) {
         return false;
     }
 
     @Override
     public boolean remove(Vertex vertex) {
-        return false;
+        return vertices.remove(vertex);
+    }
+
+    @Override
+    public String toString(){
+        return this.getClass().getSimpleName() + "[" + vertices + "]";
     }
 }
